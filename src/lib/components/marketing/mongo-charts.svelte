0d782afd4dd080f5b7346520d911e73503f0d27b
Kohei Yoshino <kohei.yoshino@gmail.com>
2023-01-10 11:15:17 -0500

    統計情報チャートを更新
    Fix https://github.com/webdino/vm.webdino.org/issues/112

--- src/lib/components/marketing/mongo-charts.svelte
+++ src/lib/components/marketing/mongo-charts.svelte
@@ -4,6 +4,8 @@
   `<MongoChartsRenderer>` component, which can be included only once in the stats page layout.
 -->
 <script>
+  import { _ } from 'svelte-i18n';
+
   /**
    * Chart list, including the IDs and optional cache expires.
    * @type {Array.<Object>}
@@ -11,6 +13,23 @@
   export let charts = [];
 </script>
 
-{#each charts as { id, cache }}
-  <div data-chart-id={id} data-cache={cache} />
+{#each charts as { id, cache, note }}
+  <section>
+    <div data-chart-id={id} data-cache={cache} />
+    {#if note}
+      <div role="note">{$_('pages._global.misc.note_prefix')} {note}</div>
+    {/if}
+  </section>
 {/each}
+
+<style lang="scss">
+  section {
+    margin: 48px 0 0;
+  }
+
+  [role='note'] {
+    color: #4e6e8e;
+    font-size: 0.9rem;
+    text-align: center;
+  }
+</style>
