098a157770951ebb727e080f308491beb726846e
Kohei Yoshino <kohei.yoshino@gmail.com>
2024-06-16 21:45:36 -0400

    Update dependencies

--- src/lib/services/util/markdown.js
+++ src/lib/services/util/markdown.js
@@ -5,12 +5,20 @@ import { marked } from 'marked';
 const renderer = new marked.Renderer();
 const linkRenderer = renderer.link;
 
-renderer.link = (href, title, text) => {
-  const html = linkRenderer.call(renderer, href, title, text);
+/**
+ * @param {import('marked').Tokens.Link} args
+ * @returns {string}
+ */
+renderer.link = (args) => {
+  const html = linkRenderer.call(renderer, args);
 
-  return href.startsWith('/')
+  return args.href.startsWith('/')
     ? html
     : html.replace(/^<a /, `<a target="_blank" rel="noreferrer noopener nofollow" `);
 };
 
-export const parse = (markdown) => marked(markdown, { renderer });
+/**
+ * @param {string} markdown
+ * @returns {string}
+ */
+export const parse = (markdown) => /** @type {string} */ (marked(markdown, { renderer }));
