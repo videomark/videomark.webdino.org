e4a57b28fb03b68af2aa3ac75c82bd5113439ea6
Kohei Yoshino <kohei.yoshino@gmail.com>
2025-12-17 05:50:39 -0500

    利用規約とプライバシーポリシーの更新 (#13)

--- src/routes/(marketing)/[locale=locale]/(default)/privacy/+page.svelte
+++ src/routes/(marketing)/[locale=locale]/(default)/privacy/+page.svelte
@@ -1,7 +1,9 @@
 <script>
   import { _, json } from 'svelte-i18n';
   import Article from '$lib/components/marketing/article.svelte';
+  import LegalDocHistory from '$lib/components/marketing/legal-doc-history.svelte';
   import MetaTags from '$lib/components/marketing/meta-tags.svelte';
+  import { unique } from '$lib/services/util/array';
   import { parse } from '$lib/services/util/markdown';
 </script>
 
@@ -11,3 +13,8 @@
   <h1>{$_('pages.privacy.intro.heading')}</h1>
   {@html parse($_('pages.privacy.body'))}
 </Article>
+
+<LegalDocHistory
+  title={$_('pages.privacy.history.heading')}
+  changes={unique(/** @type {object[]} */ ($json('pages.privacy.history.changes')), 'date')}
+/>
